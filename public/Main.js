6965ea9445353d2298c682a6f9db8f74ddfaceb6
EarvinKayonga <demoneartin@gmail.com>
2016-10-10 10:13:15 +0200

    no job

--- public/Main.js
+++ public/Main.js
@@ -1,112 +1,120 @@
+function wrtc(initiator) {
+    navigator.getUserMedia({
+        video: true,
+        audio: true,
+    }, gotMedia, function() {});
+
+    function gotMedia(stream) {
+        var peer = new SimplePeer({
+            initiator: initiator,
+            trickle: false,
+            stream: stream
+        })
+
+
+        peer.on('signal', function(data) {
+            document.getElementById('yourId').innerText = JSON.stringify(data)
+        })
+
+        /*  document.getElementById('connect').addEventListener('click', function() {
+              var otherId = JSON.parse(document.getElementById('otherId').value)
+              console.log(otherId);
+              peer.signal(otherId);
+          })*/
+
+
+        peer.on('stream', function(stream) {
+            var video = document.createElement('video')
+            document.body.appendChild(video)
+
+            video.src = window.URL.createObjectURL(stream)
+            video.play()
+        })
+    }
+}
+
+function notifyFailure() {
+    swal({
+        title: "Oups",
+        text: "The Browser you using doesn't support WEBRTC",
+        imageUrl: "img/web-rtc.png"
+    });
+};
+
+function run() {
+    let ws = new WebSocket('wss://' + window.document.location.host);
+    ws.onmessage = (event) => {
+        message = JSON.parse(event.data)
+        var initiator = false
+        var userName = ''
+
+
+        var p1 = new Promise(function(resolve, reject) {
+            swal({
+                    title: "Who are u ?",
+                    text: "Choose a username:",
+                    type: "input",
+                    showCancelButton: false,
+                    closeOnConfirm: false,
+                    animation: "slide-from-top",
+                    inputPlaceholder: "Choose a username"
+                },
+                (usrNm) => {
+                    if (usrNm === false || usrNm === "" || ifUserTaken(usrNm)) {
+                        swal.showInputError("You need to write something!");
+                        return (false);
+                    } else {
+                        setTimeout(() => {
+                            userName = usrNm
+                            resolve(usrNm)
+                        }, 1000);
+                        swal("Nice!", "You wrote: " + usrNm, "success");
+                    };
+                });
+        })
+
+
+
+        if (message && message.connected) {
+            return p1.then(
+                (userName) => {
+                    console.log(userName);
+                    return wrtc(message.connected <= 1, userName)
+                }
+            ).catch(
+                (e) => {
+                    console.log(e)
+                }
+            )
+        } else {
+            switch (message) {
+                case message:
+                    //console.log(message);
+                    break;
+                default:
+            }
+        }
+    };
 
 
 
 
-function wrtc(){
-   navigator.getUserMedia({
-       video: true,
-       audio: true,
-   }, gotMedia, function() {});
 
-   function gotMedia(stream) {
-       var peer = new SimplePeer({
-           initiator: location.hash === '#init',
-           trickle: false,
-           stream: stream
-       })
+    function ifUserTaken(username) {
+        //Check if is Taken and set in Level
+        return (false);
+    };
 
 
-       peer.on('signal', function(data) {
-           document.getElementById('yourId').innerText = JSON.stringify(data)
-       })
+}
 
-     /*  document.getElementById('connect').addEventListener('click', function() {
-           var otherId = JSON.parse(document.getElementById('otherId').value)
-           console.log(otherId);
-           peer.signal(otherId);
-       })*/
+function App(username) {
 
+    //return getUsers();
+};
 
-       peer.on('stream', function(stream) {
-           var video = document.createElement('video')
-           document.body.appendChild(video)
+(function() {
+    "use strict";
 
-           video.src = window.URL.createObjectURL(stream)
-           video.play()
-       })
-   }
- }
-
- function getUsers(){
-   return axios.get('/users/all')
-         .then(
-           (users) => {
-             return
-              users.forEach((user) => {
-               let nodeElm = document.createElement('span'),
-                   text  = document.createTextNode(user.key),
-                   btn = document.createElement("BUTTON"),
-                   t = document.createTextNode("Call");
-
-                 let chat = document.getElementById('chat');
-
-                 nodeElm.appendChild(text);
-                 btn.appendChild(t);
-                 chat.appendChild(nodeElm);
-
-                 console.log(user);
-             });
-           }
-         )
-         .catch(
-           (error) => {
-             console.log(error);
-           }
-         );
- };
-
- function notifyFailure() {
-     swal({
-         title: "Oups",
-         text: "The Browser you using doesn't support WEBRTC",
-         imageUrl: "img/web-rtc.png"
-       });
- };
-
- function run() {
-     let ws = new WebSocket('wss://' + window.document.location.host);
-
-     function ifUserTaken(username) {
-       //Check if is Taken and set in Level
-       return (false);
-     };
-
-     swal({
-       title: "Who are u ?",
-       text: "Choose a username:",
-       type: "input",
-       showCancelButton: false,
-       closeOnConfirm: false,
-       animation: "slide-from-top",
-       inputPlaceholder: "Choose a username" },
-       (usrNm) => {
-         if (usrNm === false || usrNm === "" || ifUserTaken(usrNm)){
-           swal.showInputError("You need to write something!");
-           return (false);
-         } else {
-           swal("Nice!", "You wrote: " + usrNm, "success");
-           setTimeout(() => {
-             return App(usrNm);
-           }, 1000);
-         };
-       });
- }
-
- function App(username){
-   return getUsers();
- };
-
- (function() {
-     "use strict";
-     return (SimplePeer.WEBRTC_SUPPORT || typeof(Worker) == "undefined") ? run() : notifyFailure();
- }());
+    return (SimplePeer.WEBRTC_SUPPORT || typeof(Worker) == "undefined") ? run() : notifyFailure();
+}());
--- worker/index.js
+++ worker/index.js
@@ -8,23 +8,24 @@ const port    = process.env.PORT || 3002,
       ws      = require('ws'),
       express = require('express'),
       url     = require('url'),
-      bodyParser = require('body-parser'),
       app     = express(),
 
-      user    = require('../domain/websocket/User.js'),
+
       authorize = require('../domain/websocket/index.js').authorize,
-      userRouter = require('../domain/User.js'),
 
       WebSocketServer = ws.Server;
 
 let errorMessage = 'UnAuthorized';
 
 function assign(server, fn){
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use('/users', userRouter);
+  let wss = new WebSocketServer({ server: server,  autoAcceptConnections: true });
 
-  let wss = new WebSocketServer({ server: server });
+  wss.broadcast = function broadcast(data) {
+      wss.clients.forEach(function each(client) {
+        client.send(data);
+      });
+  };
+
   wss.on('connection', (ws) => {
     let auth = ws.upgradeReq.headers["sec-websocket-key"]
 
@@ -32,35 +33,15 @@ function assign(server, fn){
       console.log('received: %s', message);
     });
 
-    //ws.send('something');
 
-    ws.on(user.create.url, (message) => {
-      return user.create.handle(message, () => {
-        return (ws.upgradeReq.headers["sec-websocket-key"]);
-      });
-    });
-
-    ws.on(user.update.url, () => {
-      return user.update.handle(message, () => {
-        return (authorize(auth, message)) ? authorize(auth, message) : ws.send(errorMessage) || authorize(auth, message);
-      });
-    });
-
-    ws.on(user.read.url, () => {
-      return user.read.handle(message, () => {
-        return (authorize(auth, message)) ? authorize(auth, message) : ws.send(errorMessage) || authorize(auth, message);
-      });
-    });
-    ws.on(user.delete.url, () => {
-      return user.delete.handle(message, () => {
-        return (authorize(auth, message)) ? authorize(auth, message) : ws.send(errorMessage) || authorize(auth, message);
-      });
-    });
+    wss.broadcast(`{"connected" :${wss.clients.length}}`)
 
     ws.on('close', () => {
-      console.log('stopping client interval');
+
     });
   });
+
+
 
 
   fn();
